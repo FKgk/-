23d5c57c40cc72eb429f6e01da2b56d7f313ec8b
FKgk <rhkd865@naver.com>
2019-03-29 20:31:08 +0900

    add special functions, history and modify copy&paste

--- HW1/p1.js
+++ HW1/p1.js
@@ -2,35 +2,55 @@ var result = document.getElementById('result');
 var input = document.getElementById('input');
 var enter = document.getElementById('enter');
 var clear = document.getElementById('clear');
-var numberButtons = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-var operations = ['+', '-', '*', '/', '%', '.', '(', ')', '[', ']', ','];
-var operationButtons = [];
-var copyString = "";
 var copyApaste = document.getElementById('copyApaste');
 var backSpace = document.getElementById('backSpace');
+var historyList = document.getElementById('history-tablelist');
 
-for (let i = 0; i < numberButtons.length; i++) {
-    numberButtons[i] = document.getElementById("n" + i);
+var numbers = document.getElementsByClassName('number');
+var normalOperations = document.getElementsByClassName('normalOperation');
+var specialOperations = document.getElementsByClassName('specialOperation');
+var historyCount = 0;
+var copyString = "";
 
-    numberButtons[i].addEventListener('click', function () {
-        input.value += numberButtons[i].value;
-        console.log("numberButtons[" + i + "] (" + numberButtons[i].value + ") : input.value = " + input.value);
+
+for (let i = 0; i < numbers.length; i++) {
+    numbers[i].addEventListener('click', function () {
+        input.value += numbers[i].value;
+        console.log("numberButtons[" + i + "] (" + numbers[i].value + ") : input.value = " + input.value);
     });
 }
 
-for (let i = 0; i < operations.length; i++) {
-    operationButtons[i] = document.getElementById("o" + i);
-
-    operationButtons[i].addEventListener('click', function () {
-        input.value += operations[i];
-        console.log("operationButtons[" + i + "] '" + operations[i] + "' : input.value = " + input.value);
+for (let i = 0; i < normalOperations.length; i++) {
+    normalOperations[i].addEventListener('click', function () {
+        input.value += normalOperations[i].value;
+        console.log("normalOperations[" + i + "] '" + normalOperations[i].value + "' : input.value = " + input.value);
     });
+}
+for (let i = 0; i < specialOperations.length; i++)
+{
+    specialOperations[i].addEventListener('click', function(){
+        input.value += specialOperations[i].value + "(";
+        console.log("specialOperations[" + i + "] '" + specialOperations[i].value + "' : input.value = " + input.value);
+    })
 }
 
 enter.addEventListener('click', function () {
     try{
-        result.innerHTML = "&#61;" + math.eval(input.value);
-        console.log("enter : result.value = " + result.innerHTML);
+        let answer = math.eval(input.value);
+        result.innerHTML = "&#61;" + answer;
+        try{
+            let value = input.value + " &#61; " + answer.toString();
+            let onclickStr = "input.value = \"" + input.value  + "\"; result.innerHTML = \"" + answer + "\";";
+
+            historyList.innerHTML += "<tr><input type='button' class='btn btn-default' id='history" + historyCount + "' data-dismiss='modal' style='background-color: white;' onclick=\"input.value='" + input.value + "'; result.innerHTML = '" + answer + "'\" value='" + value + "'></tr>";
+        }
+        catch(ex){
+            console.log("enter Error : " + ex.message);
+            console.log(historyList.innerHTML);
+            alert("history saved Error");
+        }
+
+        console.log("enter : result.value = " + result.innerHTML.substr(1, result.innerHTML.length - 1));
     }
     catch(ex){
         console.log("enter Error : " + ex.message);
@@ -43,30 +63,27 @@ clear.addEventListener('click', function () {
     input.value = "";
     console.log("clear : input.value = " + input.value);
 });
+
 copyApaste.addEventListener('click', function () {
     let txt = "";
     if (window.getSelection) {
         txt = window.getSelection();
         console.log("window.getSelection : txt = " + txt);
-    } else {
-        /*else if (document.getSelection) {
-        txt = document.getSelection();
-    } else if (document.selection) {
-        txt = document.selection.createRange().text;
-    }*/
-    console.log("copyApaste Error wrong detection windo.getSelection == false");
-    }
 
-    if (txt.toString() == "" || txt.toString() == copyString) {
-        input.value = input.value + "" + copyString;
+        if (txt.toString() == "" || txt.toString() == copyString) {
+            input.value = input.value + "" + copyString;
 
-        console.log("paste : input.value = " + input.value);
-    }
+            console.log("paste : input.value = " + input.value);
+        }
+        else {
+            copyString = txt.toString();
+
+            console.log("copy : copyString = " + copyString);
+        }
+    } 
     else {
-        copyString = txt.toString();
-        
-        console.log("copy : copyString = " + copyString);
-    }
+    console.log("copyApaste Error wrong detection windo.getSelection == false");
+    }    
 });
 
 backSpace.addEventListener('click', function () {
@@ -74,3 +91,5 @@ backSpace.addEventListener('click', function () {
 
     console.log("backSpace input.value : " + input.value);
 });
+
+
