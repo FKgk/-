c68fc2e4c43243a6efb02f794669d76156f611a8
FKgk <rhkd865@naver.com>
2019-03-30 23:23:40 +0900

    later add variable and function

--- HW1/p1.js
+++ HW1/p1.js
@@ -12,56 +12,72 @@ var specialOperations = document.getElementsByClassName('specialOperation');
 var historyCount = 0;
 var copyString = "";
 
+var calFormula = math.parser();
+
 
 for (let i = 0; i < numbers.length; i++) {
     numbers[i].addEventListener('click', function () {
-        input.value += numbers[i].value;
-        console.log("numberButtons[" + i + "] (" + numbers[i].value + ") : input.value = " + input.value);
+        input.innerHTML += numbers[i].value;
+        console.log("numberButtons[" + i + "] (" + numbers[i].value + ") : input.innerHTML = " + input.innerHTML);
     });
 }
 
 for (let i = 0; i < normalOperations.length; i++) {
     normalOperations[i].addEventListener('click', function () {
-        input.value += normalOperations[i].value;
-        console.log("normalOperations[" + i + "] '" + normalOperations[i].value + "' : input.value = " + input.value);
+        input.innerHTML += normalOperations[i].value;
+        console.log("normalOperations[" + i + "] '" + normalOperations[i].value + "' : input.value = " + input.innerHTML);
     });
 }
 for (let i = 0; i < specialOperations.length; i++)
 {
     specialOperations[i].addEventListener('click', function(){
-        input.value += specialOperations[i].value + "(";
-        console.log("specialOperations[" + i + "] '" + specialOperations[i].value + "' : input.value = " + input.value);
-    })
+        input.innerHTML += specialOperations[i].value + "(";
+        console.log("specialOperations[" + i + "] '" + specialOperations[i].value + "' : input.value = " + input.innerHTML);
+    });
 }
 
 enter.addEventListener('click', function () {
     try{
-        let answer = math.eval(input.value);
-        result.innerHTML = "&#61;" + answer;
-        try{
-            let value = input.value + " &#61; " + answer.toString();
-            let onclickStr = "input.value = \"" + input.value  + "\"; result.innerHTML = \"" + answer + "\";";
-
-            historyList.innerHTML += "<tr><input type='button' class='btn btn-default' id='history" + historyCount + "' data-dismiss='modal' style='background-color: white;' onclick=\"input.value='" + input.value + "'; result.innerHTML = '" + answer + "'\" value='" + value + "'></tr>";
+        let answer = calFormula.eval(input.innerHTML).toString();
+        
+        if(answer.split(' ')[0] == 'function'){
+            answer = answer.split(' ');
         }
-        catch(ex){
-            console.log("enter Error : " + ex.message);
-            console.log(historyList.innerHTML);
-            alert("history saved Error");
-        }
+        else{
+            try {
+                let value ="";
+                if (input.innerHTML.match('=') && 
+                    (!input.innerHTML.match('!=') && !input.innerHTML.match('>=') && !input.innerHTML.match('<=')))
+                    {
+                        value = input.innerHTML.replace('=', ' = ');
+                    }
+                    else{
+                        value = input.innerHTML + " &#61; " + answer.toString();
+                    }
 
-        console.log("enter : result.value = " + result.innerHTML.substr(1, result.innerHTML.length - 1));
+                historyList.innerHTML += "<tr><button type='button' class='btn btn-outline-secondary history_list' id='history" + historyCount + "' data-dismiss='modal' onclick=\"input.innerHTML='" + input.innerHTML + "'; result.innerHTML = '" + answer + "'\">" + value + "</button></tr>";
+            }
+            catch (ex) {
+                console.log("enter Error : " + ex.message);
+                console.log(historyList.innerHTML);
+                alert("history saved Error");
+            }
+        }
+        result.innerHTML = answer;
+        
+
+        console.log("enter : result.value = " + result.innerHTML);
     }
     catch(ex){
         console.log("enter Error : " + ex.message);
-        console.log("input.value = " + input.value);
+        console.log("input.innerHTML = " + input.innerHTML);
         alert("Please check the formula \nThis formula can't be calculated.");
     }
 });
 
 clear.addEventListener('click', function () {
-    input.value = "";
-    console.log("clear : input.value = " + input.value);
+    input.innerHTML = "";
+    console.log("clear : input.innerHTML = " + input.innerHTML);
 });
 
 copyApaste.addEventListener('click', function () {
@@ -71,9 +87,9 @@ copyApaste.addEventListener('click', function () {
         console.log("window.getSelection : txt = " + txt);
 
         if (txt.toString() == "" || txt.toString() == copyString) {
-            input.value = input.value + "" + copyString;
+            input.innerHTML = input.innerHTML + "" + copyString;
 
-            console.log("paste : input.value = " + input.value);
+            console.log("paste : input.innerHTML = " + input.innerHTML);
         }
         else {
             copyString = txt.toString();
@@ -87,9 +103,12 @@ copyApaste.addEventListener('click', function () {
 });
 
 backSpace.addEventListener('click', function () {
-    input.value = input.value.substr(0, input.value.length - 1);
+    input.innerHTML = input.innerHTML.substr(0, input.innerHTML.length - 1);
 
-    console.log("backSpace input.value : " + input.value);
+    console.log("backSpace input.innerHTML : " + input.innerHTML);
 });
 
-
+/* 
+모달 안 꺼지게
+$('#newModal').modal({ backdrop: 'static', keyboard: false }); 
+*/
